5a29a40c1e5e69dc3e318876b901d718cfd1a404
mydearlyla <xtolove@naver.com>
2022-11-16 02:34:27 +0900

    1116 AM 2:33 회원 1차 병합

--- project_taiso/src/main/java/com/taiso/notice/action/AdminNoticeDeleteAction.java
+++ project_taiso/src/main/java/com/taiso/notice/action/AdminNoticeDeleteAction.java
@@ -1,13 +1,11 @@
-package com.taiso.admin_notice.action;
+package com.taiso.notice.action;
 
 import java.io.PrintWriter;
-
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.taiso.admin_notice.db.BoardDTO;
-import com.taiso.admin_notice.db.noticeDAO;
+import com.taiso.notice.db.noticeDAO;
 
 public class AdminNoticeDeleteAction implements Notice {
 
--- project_taiso/src/main/java/com/taiso/notice/action/AdminNoticeDetailAction.java
+++ project_taiso/src/main/java/com/taiso/notice/action/AdminNoticeDetailAction.java
@@ -1,44 +1,30 @@
-package com.taiso.admin_notice.action;
+package com.taiso.notice.action;
 
 import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
 
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
-import com.taiso.admin_notice.db.BoardDTO;
-import com.taiso.admin_notice.db.noticeDAO;
+import com.taiso.notice.db.BoardDTO;
+import com.taiso.notice.db.noticeDAO;
 
 public class AdminNoticeDetailAction implements Notice {
 
 	@Override
 	public NoticeForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
 		System.out.println(" M : AdminNoticeDetailAction_execute() 호출");
 		
-		// 1) 파일 업로드
-		// 업로드 가상폴더 생성 /upload
-		String realPath = request.getRealPath("./upload");
-		System.out.println(" M : realPath : " + realPath);
-		// 첨부파일 크기 지정 / 10MB
-		int maxSize = 10 * 1024 * 1024;
-		
-		// 파일업로드 -> 파일업로드 객체 생성(MultipartRequest)
-		MultipartRequest multi 
-							= new MultipartRequest(
-									request,
-									realPath,
-									maxSize,
-									"UTF-8",
-									new DefaultFileRenamePolicy()
-									);
-		
 		// 전달정보(파라메터) 저장
-		int bo_num = Integer.parseInt(multi.getParameter("bo_num"));
-		String pageNum = multi.getParameter("pageNum");
+		int bo_num = Integer.parseInt(request.getParameter("bo_num"));
+		String pageNum = request.getParameter("pageNum");
 		
 				
 		noticeDAO dao = new noticeDAO();
 		// 글 조회수 1증가 -> DAO 1증가 메서드 호출
 		dao.updateReadcount(bo_num);
+		
 		System.out.println(" M : 조회수 1증가 완료! ");
 		
 		// 글 가져오기
@@ -50,7 +36,7 @@ public class AdminNoticeDetailAction implements Notice {
 		
 		// 페이지 이동(준비)
 		NoticeForward forward = new NoticeForward();
-		forward.setPath("./admin_notice/adminNoticeDetail.jsp");
+		forward.setPath("./notice/adminNoticeDetail.jsp");
 		forward.setRedirect(false);
 		
 		return forward;
